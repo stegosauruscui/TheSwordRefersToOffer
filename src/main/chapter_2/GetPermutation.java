b750007244d32dd65f63369e3a2336a27263281c
崔剑龙 <cuijianlong@meituan.com>
2017-05-04 15:06:54 +0800

    增加ignore

---
+++ src/main/chapter_2/GetPermutation.java
@@ -0,0 +1,39 @@
+package main.chapter_2;
+
+import java.util.ArrayList;
+
+/**
+ * Created by cuijl on 12/19/16.
+ * 给定 n 和 k，求123..n组成的排列中的第 k 个排列,1 ≤ n ≤ 9
+ *
+ * 样例：
+ * 对于 n = 3, 所有的排列如下：
+ * (1) 123
+ * (2) 132
+ * (3) 213
+ * (4) 231
+ * (5) 312
+ * (6) 321
+ *
+ * 如果 k = 4, 第4个排列为，231.
+ */
+public class GetPermutation {
+
+
+    public String getPermutation(int n, int k){
+
+        //for(int i= 0; i < k; ++i){
+        //
+        //}
+        return "";
+
+    }
+
+    public void Permutation(char[] str){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
