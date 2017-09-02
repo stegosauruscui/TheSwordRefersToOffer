0cb41aa7f2960526fb0426a7809351f0937719d4
崔剑龙 <cuijianlong@meituan.com>
2017-09-02 18:11:02 +0800

    test

---
+++ src/main/chapter_2/ReverseList.java
@@ -0,0 +1,34 @@
+package main.chapter_2;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Description: 翻转链表
+ * 给出一个链表1->2->3->null，这个翻转后的链表为3->2->1->null
+ * @Author: cuijianlong
+ * @Created: 6/14/17  10:48 PM
+ */
+
+public class ReverseList {
+
+  public class ListNode {
+      int val;
+      ListNode next;
+
+      ListNode(int val) {
+          this.val = val;
+          this.next = null;
+      }
+  }
+
+  public ListNode reverse(ListNode head) {
+
+
+    return null;
+  }
+
+  public static void main(String[] args){
+
+  }
+
+}
